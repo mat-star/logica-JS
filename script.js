10aadea266e73bfb25bbf05978a6624d8c55e73a
mat-star <mateus.silva.jesus@escola.pr.gov.br>
2024-08-28 08:00:20 -0300

    js

---
+++ script.js
@@ -0,0 +1,24 @@
+var nome = 'João';
+let idade = 25;
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b;
+console.log(soma)
+
+function multiplica (a, b){
+    return a * b ;
+}
+let produto = multiplica(a, b);
+console.log(produto);
+
+let frutas = ['apple', 'Banana', 'cherry'];
+console.log(frutas[1]);
+
+let pessoa = {
+    primeiroNome: 'Joao',
+    Sobrenome; 'Delon',
+    idade; 25,
+};
+console.log(pessoa.primeiroNome);
